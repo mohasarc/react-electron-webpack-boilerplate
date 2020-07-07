b2d4eeb75c50af0e01d070df2fe59a66992c18cf
Mohammed S. Yaseen <moha.98.1900@gmail.com>
2020-07-07 17:27:13 +0300

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+// Import dependencies
+import React from 'react'// Create main App component
+
+const App = () => (
+  <div>
+    <p>HIIIIIII</p>
+  </div>
+);
+
+// Export the App component
+export default App
